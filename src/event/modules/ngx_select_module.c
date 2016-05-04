eefa00326c0b8f135b9c8263b20e904d34dd54bd
Balakrishna Garapati <balakrishna.garapati@linaro.org>
2016-05-04 14:52:56 +0200

    event: modules: support odp 1.7
    Signed-off-by: Balakrishna Garapati <balakrishna.garapati@linaro.org>

--- src/event/modules/ngx_select_module.c
+++ src/event/modules/ngx_select_module.c
@@ -322,12 +322,12 @@ ngx_select_process_events(ngx_cycle_t *cycle, ngx_msec_t timer,
 	odp_event_t odp_ev;
 	odp_packet_t pkt;
 	odp_queue_t in_queue;
-	odp_event_t events[OFP_PKT_SCHED_MULTI_EVENT_SIZE];
+	odp_event_t events[OFP_EVT_RX_BURST_SIZE];
 	int event_idx = 0;
 	int event_cnt = 0;
 
 	event_cnt = odp_schedule_multi(&in_queue, ODP_SCHED_NO_WAIT,
-			 events, OFP_PKT_SCHED_MULTI_EVENT_SIZE);
+			 events, OFP_EVT_RX_BURST_SIZE);
 	for (event_idx = 0; event_idx < event_cnt; event_idx++) {
 		odp_ev = events[event_idx];
 
